8aefae00fe537888e35654a77ff9d799b980b381
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-20 22:49:00 +0300

    Объекты_И_Классы

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Объекты_И_Классы");
 
-        Author author = new Author("Александр", "Дюма");
-        System.out.println(author);
+        Author author = new Author("Айзек", "Азимов");
+        System.out.println(author.getName()+" "+author.getSurname()+" ");
+       // Book book = new Book("Двухсотлетний человек",);
+       // System.out.println(book);
     }
 }
