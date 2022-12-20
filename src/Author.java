a8590a85269c6587bdca8927239c742cf0fabfad
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-20 22:14:45 +0300

    Объекты_И_Классы

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@ public class Author {
     public String name;
     public String surname;
 
-    public  Author(String name, String surname) {
+    public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
