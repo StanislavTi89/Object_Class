7c889dbe7812b482dfbec1b94c45dd874d61cfca
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-20 21:30:12 +0300

    Объекты_И_Классы

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,37 @@
 
 public class Book {
     //содержит в себе данные об имени, авторе и годе публикации
+    public String title;
+    public Author author;
+    public int published;
 
+    public Book (String title, Author author, int published) {
+        this.title = title;
+        this.author = author;
+        this.published = published;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public int getPublished() {
+        return published;
+    }
+
+    public void setPublished(int published) {
+        this.published = published;
+    }
 }
